c0c3ccf123d4cd52334e40e30d439df6e26b83f1
Oualid <yabonees@gmail.com>
2023-09-19 19:21:39 +0200

    Routing and homepage

--- Source/Project C/ClientApp/src/App.tsx
+++ Source/Project C/ClientApp/src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Parent from './Parent'
+import Header from './components/Header'
+import {BrowserRouter as Router ,Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-        <h1 className='text-green-500'>Test</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      <h1 className='bg-red-500'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <>
+  <Router>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Parent/>}>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/authentication" element={<LoginPage/>} />
+      </Route>
+
+    </Routes>
+  </Router>
+  </>
   )
 }
 
---
+++ Source/Project C/ClientApp/src/pages/HomePage.tsx
@@ -0,0 +1,20 @@
+
+const HomePage = () => {
+  return (
+    <div className="h-[100vh] flex flex-col">
+        <div className="background_image"></div>
+        
+        <div>
+            <div className='main flex flex-col justify-center h-screen w-full'>
+                <h1 className="-mt-100 ml-32">Viscon group machine support</h1>
+                <p className="ml-32 text-[#00c1e3] font-[700] text-lg">We solve your problems!</p>
+            </div>  
+
+        </div>
+
+    </div>
+
+  )
+}
+
+export default HomePage
