854a19272c1877342b1dc0fab74776d9b68cf358
Oualid <yabonees@gmail.com>
2023-09-26 11:52:07 +0200

    background imge change

--- Source/Project C/ClientApp/src/components/Header.tsx
+++ Source/Project C/ClientApp/src/components/Header.tsx
@@ -8,12 +8,12 @@ const Header = () => {
   return (
     <div className='min-h-[60px] absolute top-0 right-0 left-0 flex items-center justify-between bg-white p-1 border-b-8 border-[#009fe3]'>
         <img src={logo} alt="logo" className="h-[50px] max-w-[150px] cursor-pointer hover:scale-95" onClick={() => navigate("/")} />
-        <div className='flex justify-between w-[60%]'>
+        <div className='flex justify-between w-[50%]'>
           <ul className='flex text-black gap-8 items-center text-lg'>
-            <li>Create ticket</li> | 
-            <li>FAQ</li> | 
-            <li>About us</li> | 
-            <li>Contact us</li>
+            <li className='cursor-pointer'>Create ticket</li> | 
+            <li className='cursor-pointer'>FAQ</li> | 
+            <li className='cursor-pointer'>About us</li> | 
+            <li className='cursor-pointer'>Contact us</li>
           </ul>
           <svg xmlns="http://www.w3.org/2000/svg" fill="#009fe3" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-14 cursor-pointer"
             onClick={() => navigate("/authentication")}
