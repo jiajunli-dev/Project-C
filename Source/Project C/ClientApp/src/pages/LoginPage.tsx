f1be7473ec29ace1412608e65a535adf4b7061bd
iHesh84 <1058647@hr.nl>
2023-09-26 12:00:17 +0200

    test auth

--- Source/Project C/ClientApp/src/pages/LoginPage.tsx
+++ Source/Project C/ClientApp/src/pages/LoginPage.tsx
@@ -1,14 +1,30 @@
-import { ClerkProvider } from "@clerk/clerk-react";
-
 const clerkPubKey =
   "pk_test_c3RlcmxpbmctbW9yYXktOTAuY2xlcmsuYWNjb3VudHMuZGV2JA";
+
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
 
 function LoginPage() {
   return (
     <ClerkProvider publishableKey={clerkPubKey}>
-      <div className="bg-red-500">Hello from clerk</div>
+      <SignedIn>
+        <Welcome />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
     </ClerkProvider>
   );
+}
+
+function Welcome() {
+  return <div>Hello you are signed in</div>;
 }
 
 export default LoginPage;
