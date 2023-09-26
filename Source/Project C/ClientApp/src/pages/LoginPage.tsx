fdc485fd2439f571a70f0d2528a6cb1d91586227
iHesh84 <1058647@hr.nl>
2023-09-26 11:54:52 +0200

    added auth

--- Source/Project C/ClientApp/src/pages/LoginPage.tsx
+++ Source/Project C/ClientApp/src/pages/LoginPage.tsx
@@ -1,9 +1,14 @@
-const LoginPage = () => {
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const clerkPubKey =
+  "pk_test_c3RlcmxpbmctbW9yYXktOTAuY2xlcmsuYWNjb3VudHMuZGV2JA";
+
+function LoginPage() {
   return (
-    <div className='flex items-center justify-center text-black'>
-        Login Page 
-    </div>
-  )
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <div className="bg-red-500">Hello from clerk</div>
+    </ClerkProvider>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
