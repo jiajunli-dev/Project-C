a4950958b8f03ecef41db4bd78c40e419cd7b2ab
iHesh84 <1058647@hr.nl>
2023-10-10 15:12:23 +0200

    commit to pc

--- Source/Project C/ClientApp/tailwind.config.js
+++ Source/Project C/ClientApp/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   darkMode: "class",
   theme: {
     extend: {
@@ -9,5 +13,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
