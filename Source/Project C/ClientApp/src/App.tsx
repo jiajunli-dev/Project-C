7ca6c6b2b9c13a9dea5fbb53f530359b47c20828
Oualid <yabonees@gmail.com>
2023-10-10 14:33:15 +0200

    Create ticket start

--- Source/Project C/ClientApp/src/App.tsx
+++ Source/Project C/ClientApp/src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import CreateTicket from "./pages/CreateTicket";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Parent />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/authentication" element={<LoginPage />} />
+            <Route path="/create-ticket" element={<CreateTicket />} />
           </Route>
         </Routes>
       </Router>
