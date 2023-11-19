fd65fec02f10dc465b699f898b5a3c6cdc86d245
AzifRosan <azif_rosan@hotmail.com>
2023-11-19 16:48:39 +0100

    added photoService

---
+++ Source/Project C/ClientApp/src/services/baseService.ts
@@ -0,0 +1,19 @@
+export class baseService {
+    constructor(protected baseUrl: string = "https://localhost:7004") { }
+
+    protected createRequest(method: string, token: string, endpoint: string, data?: string): Request {
+        const tokenParts = token.split('.');
+        const payload = JSON.parse(atob(tokenParts[1]));
+        const expirationTime = (payload.exp + 5) * 1000;
+
+        if (Date.now() >= expirationTime) throw new Error('Token has expired');
+
+        const headers = {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        };
+        const body = data ? data : undefined;
+
+        return new Request(`${this.baseUrl}/${endpoint}`, { method, headers, body });
+    }
+}
---
+++ Source/Project C/ClientApp/src/services/photoService.ts
@@ -0,0 +1,72 @@
+import { Photo, CreatePhoto } from "../models/Photo";
+import { baseService } from "./BaseService";
+
+export class PhotoService extends baseService {
+    // TODO: Move api url to config
+    constructor(baseUrl: string = "https://localhost:7004") { super(baseUrl); }
+
+    async getById(token: string, photoId: number): Promise<Photo | undefined> {
+        if (!token) throw new Error('Token is required.');
+        if (!photoId) throw new Error('PhotoId is required.');
+
+        const request = this.createRequest("GET", token, `Photo/${photoId}`);
+        const response = await fetch(request);
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+        const data = await response.json();
+
+        return Photo.fromJson(data);
+    }
+
+    async create(token: string, model: CreatePhoto): Promise<Photo | undefined> {
+        if (!token) throw new Error('Token is required.');
+        if (!model) throw new Error('Model is required.');
+
+        const errors = model.validate();
+        if (errors.length > 0) throw new Error(errors.join('\n'));
+
+        const request = this.createRequest("POST", token, "Photo", model.toJSON());
+        const response = await fetch(request);
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+        const data = await response.json();
+
+        return Photo.fromJson(data);
+    }
+
+    async update(token: string, model: Photo): Promise<Photo | undefined> {
+        if (!token) throw new Error('Token is required.');
+        if (!model) throw new Error('Model is required.');
+
+        const errors = model.validate();
+        if (errors.length > 0) throw new Error(errors.join('\n'));
+
+        const request = this.createRequest("PUT", token, `Photo`, model.toJSON());
+        const response = await fetch(request);
+        if (!response.ok) {
+            if (response.status === 400) {
+                const data = await response.json();
+                throw new Error(data);
+            } else if (response.status === 409) {
+                const data = await response.json();
+                throw new Error(data);
+            }
+
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        return Photo.fromJson(data);
+    }
+
+    async delete(token: string, photoId: number): Promise<void> {
+        if (!token) throw new Error('Token is required.');
+        if (!photoId) throw new Error('PhotoId is required.');
+
+        const request = this.createRequest("DELETE", token, `Photo/${photoId}`);
+        const response = await fetch(request);
+
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+}
