802591392279efc41c4847e4a8f469f28dbd7267
AzifRosan <azif_rosan@hotmail.com>
2023-11-19 16:48:57 +0100

    refactor ticketService

--- Source/Project C/ClientApp/src/services/ticketService.ts
+++ Source/Project C/ClientApp/src/services/ticketService.ts
@@ -1,37 +1,33 @@
 import { Ticket } from "../models/Ticket";
 import { CreateTicket } from "../models/CreateTicket";
 import { Photo } from "../models/Photo";
+import { baseService } from "./BaseService";
 
-export class TicketService {
-    // TODO: Move to config
-    constructor(private baseUrl: string = "https://localhost:7004") { }
+export class TicketService extends baseService {
+    // TODO: Move api url to config
+    constructor(baseUrl: string = "https://localhost:7004") { super(baseUrl); }
 
     async getAll(token: string): Promise<Ticket[] | undefined> {
+        if (!token) throw new Error('Token is required.');
+
         const request = this.createRequest("GET", token, "Ticket");
         const response = await fetch(request);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        if (response.status === 204) {
-            return [];
-        }
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+        if (response.status === 204) return [];
 
         const data = await response.json();
 
-        return data.map((ticketData: any) => {
-            return Ticket.fromJson(ticketData);
-        });
+        return data.map((ticketData: any) => Ticket.fromJson(ticketData));
     }
 
     async getById(token: string, ticketId: number): Promise<Ticket | undefined> {
+        if (!token) throw new Error('Token is required.');
+        if (!ticketId) throw new Error('TicketId is required.');
+
         const request = this.createRequest("GET", token, `Ticket/${ticketId}`);
         const response = await fetch(request);
-        if (response.status === 204 || response.status === 400) {
-            return undefined;
-        }
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+        if (response.status === 204 || response.status === 400) return undefined;
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
 
         const data = await response.json();
 
@@ -39,24 +35,27 @@ export class TicketService {
     }
 
     async getPhotosById(token: string, ticketId: number): Promise<Photo[] | undefined> {
+        if (!token) throw new Error('Token is required.');
+        if (!ticketId) throw new Error('TicketId is required.');
+
         const request = this.createRequest("GET", token, `Ticket/${ticketId}/photos`);
         const response = await fetch(request);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
 
         const data = await response.json();
-        return data.map((photo: any) => {
-            return Photo.fromJson(photo);
-        });
+        return data.map((photo: any) => Photo.fromJson(photo));
     }
 
     async create(token: string, model: CreateTicket): Promise<Ticket | undefined> {
+        if (!token) throw new Error('Token is required.');
+        if (!model) throw new Error('Model is required.');
+
+        const errors = model.validate();
+        if (errors.length > 0) throw new Error(errors.join('\n'));
+
         const request = this.createRequest("POST", token, "Ticket", model.toJSON());
         const response = await fetch(request);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
 
         const data = await response.json();
 
@@ -64,14 +63,23 @@ export class TicketService {
     }
 
     async update(token: string, model: Ticket): Promise<Ticket | undefined> {
-        var json = model.toJSON();
-        const request = this.createRequest("PUT", token, "Ticket", json);
+        if (!token) throw new Error('Token is required.');
+        if (!model) throw new Error('Model is required.');
+
+        const errors = model.validate();
+        if (errors.length > 0) throw new Error(errors.join('\n'));
+
+        const request = this.createRequest("PUT", token, "Ticket", model.toJSON());
         const response = await fetch(request);
         if (!response.ok) {
             if (response.status === 400) {
                 const data = await response.json();
                 throw new Error(data);
+            } else if (response.status === 409) {
+                const data = await response.json();
+                throw new Error(data);
             }
+
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
@@ -80,28 +88,13 @@ export class TicketService {
         return Ticket.fromJson(data);
     }
 
-    async delete(token: string, ticketId: number) {
+    async delete(token: string, ticketId: number): Promise<void> {
+        if (!token) throw new Error('Token is required.');
+        if (!ticketId) throw new Error('TicketId is required.');
+
         const request = this.createRequest("DELETE", token, `Ticket/${ticketId}`);
         const response = await fetch(request);
-        if (response.status !== 204) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-    }
 
-    private createRequest(method: string, token: string, endpoint: string, data?: string): Request {
-        const tokenParts = token.split('.');
-        const payload = JSON.parse(atob(tokenParts[1]));
-        const expirationTime = (payload.exp + 5) * 1000;
-
-        if (Date.now() >= expirationTime) {
-            throw new Error('Token has expired');
-        }
-
-        const headers = {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-        };
-        const body = data ? data : undefined;
-        return new Request(`${this.baseUrl}/${endpoint}`, { method, headers, body });
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
     }
 }
