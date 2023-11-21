dc091881dcf1340f727454d4cc2099d168142cde
RowinPelk <1053763@hr.nl>
2023-11-21 10:28:20 +0100

    API Call: Department

---
+++ Source/Project C/ClientApp/src/services/departmentService.ts
@@ -0,0 +1,80 @@
+import { CreateDepartment } from "../models/CreateDepartment";
+import { Department } from "../models/Department";
+import { baseService } from "./BaseService";
+
+export class employeeService extends baseService {
+    constructor(baseUrl: string = "https://localhost:7004") { super(baseUrl); }
+
+    async getAll(token: string): Promise<Department[] | undefined> {
+        const request = this.createRequest("GET", token, "Department");
+        const response = await fetch(request);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        if (response.status === 204) {
+            return [];
+        }
+
+        const data = await response.json();
+
+        return data.map((ticketData: any) => {
+            return Department.fromJson(ticketData);
+        });
+    }
+
+    async getById(token: string, departmentId: number): Promise<Department | undefined> {
+        const request = this.createRequest("GET", token, `Department/${departmentId}`);
+        const response = await fetch(request);
+
+        if (response.status === 204 || response.status === 400) {
+            return undefined;
+        }
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        return Department.fromJson(data);
+    }
+
+    async create(token: string, model: CreateDepartment): Promise<Department | undefined> {
+        const request = this.createRequest("POST", token, "Department", model.toJSON());
+        const response = await fetch(request);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        return Department.fromJson(data);
+    }
+
+    async update(token: string, model: Department): Promise<Department | undefined> {
+        const request = this.createRequest("PUT", token, "Department", model.toJSON());
+        const response = await fetch(request);
+
+        if (!response.ok) {
+            if (response.status === 400) {
+                const data = await response.json();
+                throw new Error(data);
+            }
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        return Department.fromJson(data);
+    }
+
+    async delete(token: string, departmentId: number): Promise<void> {
+        const request = this.createRequest("DELETE", token, `Department/${departmentId}`);
+        const response = await fetch(request);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+    }
+}
