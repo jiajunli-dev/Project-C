ec73453a4952a9a2e5a7e40601be478211d1c433
Omar <1058647@hr.nl>
2023-12-05 11:20:37 +0100

    styling

--- Source/Project C/ClientApp/src/components/FormPages/FormPageFive.tsx
+++ Source/Project C/ClientApp/src/components/FormPages/FormPageFive.tsx
@@ -1,54 +1,69 @@
-
-import { Priority } from "@/models/Priority"
-import StepCount from "./StepCount"
+import { Priority } from "@/models/Priority";
+import StepCount from "./StepCount";
 enum Status {
-    Open,
-    Closed
+  Open,
+  Closed,
 }
 
 interface FormPageFiveProps {
-    setCurrForm: React.Dispatch<React.SetStateAction<number>>
-    currForm: number
-    maxForm: number
-    ticketDescription: string
-    ticketTriedSolutions: string[]
-    ticketAdditionalNotes: string
-    ticketStatus: Status
-    ticketPriority: number
+  setCurrForm: React.Dispatch<React.SetStateAction<number>>;
+  currForm: number;
+  maxForm: number;
+  ticketDescription: string;
+  ticketTriedSolutions: string[];
+  ticketAdditionalNotes: string;
+  ticketStatus: Status;
+  ticketPriority: number;
 }
-const FormPageFive = ({ticketPriority,ticketAdditionalNotes,ticketDescription,ticketTriedSolutions, ticketStatus, currForm, maxForm}:FormPageFiveProps) => {
-
+const FormPageFive = ({
+  ticketPriority,
+  ticketAdditionalNotes,
+  ticketDescription,
+  ticketTriedSolutions,
+  ticketStatus,
+  currForm,
+  maxForm,
+}: FormPageFiveProps) => {
   return (
     <form className="w-3/5 border-2 p-4 dark:bg-[#121212]">
+      <StepCount currForm={currForm} maxForm={maxForm} />
 
-    <StepCount currForm={currForm} maxForm={maxForm} />
+      <h1 className="text-black text-lg font-bold mb-8 dark:text-white">
+        Check all of the information you have entered and make sure it is
+        correct!
+      </h1>
 
-
-    <h1 className="text-black text-lg font-bold mb-8 dark:text-white">
-        Check all of the information you have entered and make sure it is correct!
-    </h1>
-
-    <div className="flex flex-col gap-4 text-md text-black m-4">
+      <div className="flex flex-col gap-4 text-md text-black m-4">
         <h3 className="dark:text-white">
-        <span className="font-semibold dark:text-white">Description</span> :<br/> {ticketDescription}
+          <span className="font-semibold dark:text-white">Description</span> :
+          <br /> {ticketDescription}
         </h3>
         <h3>
-        <p className="text-md font-semibold dark:text-white">Tried solutions:</p>
-            {ticketTriedSolutions && 
+          <p className="text-md font-semibold dark:text-white">
+            Tried solutions:
+          </p>
+          {ticketTriedSolutions &&
             ticketTriedSolutions.map((solution) => (
-            <p className="text-md dark:text-white" key={solution}>{solution},<br /></p> 
+              <p className="text-md dark:text-white" key={solution}>
+                {solution},<br />
+              </p>
             ))}
         </h3>
         <h3 className="dark:text-white">
-        <span className="font-semibold dark:text-white">Additional notes</span> :<br/>  {ticketAdditionalNotes}
+          <span className="font-semibold dark:text-white">
+            Additional notes
+          </span>{" "}
+          :<br /> {ticketAdditionalNotes}
         </h3>
         <h3 className="dark:text-white">
-        <span className="font-semibold dark:text-white">Status :</span> {Status[ticketStatus]}
+          <span className="font-semibold dark:text-white">Status :</span>{" "}
+          {Status[ticketStatus]}
         </h3>
         <h3 className="dark:text-white">
-        <span className="font-semibold dark:text-white">Priority :</span> {Priority[ticketPriority]}
+          <span className="font-semibold dark:text-white">Priority :</span>{" "}
+          {Priority[ticketPriority]}
         </h3>
-    </div>
+      </div>
     </form>
   );
 };
